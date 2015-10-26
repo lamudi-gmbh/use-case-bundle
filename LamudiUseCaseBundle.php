37cd39326200c02f5be53849ab2cb0e4ac3ecb15
Bartosz Zasada <bartosz.zasada@lamudi.com>
2015-10-26 14:09:27 +0100

    use case container

--- LamudiUseCaseBundle.php
+++ LamudiUseCaseBundle.php
@@ -2,8 +2,26 @@
 
 namespace Lamudi\UseCaseBundle;
 
+use Lamudi\UseCaseBundle\DependencyInjection\UseCaseCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LamudiUseCaseBundle extends Bundle
 {
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new UseCaseCompilerPass());
+    }
 }
